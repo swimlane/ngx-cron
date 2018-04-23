cb8d83d9a5c36e29d176888f6e3cb8e774ab07a3
Jayson Harshbarger <hypercubed@gmail.com>
2018-04-23 16:54:38 -0600

    Fix e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('ngx-cron App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Inputs');
   });
 });
