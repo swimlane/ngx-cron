a1ebe1c3eb374b58e105619fcaca0e176426948d
Ian Schultz <2207263+ischultz@users.noreply.github.com>
2022-01-19 10:54:07 -0700

    SPT-11501 Update vulnerable NPM packages in ngx-cron (#39)
    * Update NPM packages to address known vulnerabilities
    
    * Update Angular to v13
    
    * Convert from TSLint to ESLint

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,9 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+
+import 'zone.js/testing';
 
 declare const require: {
   context(
@@ -10,13 +11,15 @@ declare const require: {
     deep?: boolean,
     filter?: RegExp
   ): {
-    keys(): string[];
     <T>(id: string): T;
+    keys(): string[];
   };
 };
 
 // First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
+  teardown: { destroyAfterEach: false }
+});
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
