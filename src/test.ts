8584978747c89d96750d69addaec284763635698
Ian Schultz <2207263+ischultz@users.noreply.github.com>
2022-01-25 23:30:50 -0700

    Revert "Convert from TSLint to ESLint"
    This reverts commit 0530e9c2d33601a6c592578aee73adec6ccf97fd.

--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,8 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
+import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-
-import 'zone.js/testing';
 
 declare const require: {
   context(
@@ -11,8 +10,8 @@ declare const require: {
     deep?: boolean,
     filter?: RegExp
   ): {
-    <T>(id: string): T;
     keys(): string[];
+    <T>(id: string): T;
   };
 };
 
