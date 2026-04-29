eafef2551f9206ef289a361ac83846cae04579c4
Jonathan Avendano <jogaj_26@hotmail.com>
2026-04-29 09:02:36 -0600

    Migrating to vitest

--- projects/swimlane/ngx-cron/src/lib/ngx-cron.module.ts
+++ projects/swimlane/ngx-cron/src/lib/ngx-cron.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { NgxCronComponent } from './ngx-cron.component';
 import { NgxCronService } from './ngx-cron.service';
 import { NgxUIModule } from '@swimlane/ngx-ui';
 
 @NgModule({
-  imports: [CommonModule, NgxUIModule],
+  imports: [CommonModule, FormsModule, NgxUIModule],
   providers: [NgxCronService],
   declarations: [NgxCronComponent],
   exports: [NgxCronComponent]
